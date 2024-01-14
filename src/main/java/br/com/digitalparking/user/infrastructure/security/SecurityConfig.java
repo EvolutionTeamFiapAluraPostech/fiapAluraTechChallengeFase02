b6603610394c946b5de8c5aa126180ba875aab84
Marcelo Akio Nishimori <mnishimori@yahoo.com.br>
2024-01-14 19:45:33 -0300

    refactor(users): the endpoint for registering users was released for use without authentication.

--- src/main/java/br/com/digitalparking/user/infrastructure/security/SecurityConfig.java
+++ src/main/java/br/com/digitalparking/user/infrastructure/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
         .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(req -> {
           req.requestMatchers(HttpMethod.POST, "/authenticate").permitAll();
+          req.requestMatchers(HttpMethod.POST, "/users").permitAll();
           req.anyRequest().authenticated();
         })
         .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
