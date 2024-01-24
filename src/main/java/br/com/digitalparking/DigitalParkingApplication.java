2cd736b90e430810ec73bcc614a391eb06c7699d
Marcelo Akio Nishimori <mnishimori@yahoo.com.br>
2024-01-23 21:36:07 -0300

    feat(parking): enable async method to parking payment notification.

--- src/main/java/br/com/digitalparking/DigitalParkingApplication.java
+++ src/main/java/br/com/digitalparking/DigitalParkingApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 @EnableFeignClients
 @ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class DigitalParkingApplication {
