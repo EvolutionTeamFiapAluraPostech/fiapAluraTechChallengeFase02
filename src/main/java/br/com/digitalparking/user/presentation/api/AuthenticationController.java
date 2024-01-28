d7fff01e5449eff1d771250b9c4a0d5882fdf42d
Marcelo Akio Nishimori <mnishimori@yahoo.com.br>
2024-01-28 15:50:21 -0300

    doc(authentication): documentation of API.

--- src/main/java/br/com/digitalparking/user/presentation/api/AuthenticationController.java
+++ src/main/java/br/com/digitalparking/user/presentation/api/AuthenticationController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/authenticate")
-public class AuthenticationController {
+public class AuthenticationController implements AuthenticationInterface {
 
   private final AuthenticationManager authenticationManager;
   private final TokenService tokenService;
